f11e7af1fba3ba0be27db6ab93a8ade61e1e450b
Tomas Silveira Salles <ts@imagineon.de>
2018-04-09 09:45:38 +0200

    Created xcode project (framework).

---
+++ TextbookRSA/TextbookRSA.h
@@ -0,0 +1,19 @@
+//
+//  TextbookRSA.h
+//  TextbookRSA
+//
+//  Created by Tomás Silveira Salles on 09.04.18.
+//  Copyright © 2018 ImagineOn GmbH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TextbookRSA.
+FOUNDATION_EXPORT double TextbookRSAVersionNumber;
+
+//! Project version string for TextbookRSA.
+FOUNDATION_EXPORT const unsigned char TextbookRSAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextbookRSA/PublicHeader.h>
+
+

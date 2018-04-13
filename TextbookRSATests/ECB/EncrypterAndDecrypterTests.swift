08be1edd6bcdc28a0f44af23500aae721c27a0f4
Tomas Silveira Salles <ts@imagineon.de>
2018-04-13 22:49:00 +0200

    Added class for encryption+decryption tests with ECB and RSA.
    Tests are still empty.

---
+++ TextbookRSATests/ECB/EncrypterAndDecrypterTests.swift
@@ -0,0 +1,18 @@
+//
+//  EncrypterAndDecrypterTests.swift
+//  TextbookRSATests
+//
+//  Created by Tomás Silveira Salles on 13.04.18.
+//  Copyright © 2018 ImagineOn GmbH. All rights reserved.
+//
+
+import XCTest
+@testable import TextbookRSA
+
+class EncrypterAndDecrypterTests: XCTestCase {
+    func testEncryptAndDecryptData() {
+    }
+    
+    func testEncryptAndDecryptText() {
+    }
+}

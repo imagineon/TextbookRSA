acce75e2665eb8ec327bda6d3241e644fb0d3705
Tomas Silveira Salles <ts@imagineon.de>
2018-04-13 20:04:40 +0200

    Implemented `ECB.reconstruct` (includes tests).

--- TextbookRSA/ECB/ECB.swift
+++ TextbookRSA/ECB/ECB.swift
@@ -24,15 +24,43 @@ public struct ECB: ECBProtocol {
         let padded = bits + [Bit.one] + Array<Bit>(repeating: .zero, count: numZeros)
         
         // Then we slice the padded array of bits into slices of size exactly `blockSize`.
-        let slices = stride(from: 0, to: padded.count, by: blockSize.value).map { index in
-            Array(padded[index ..< index + blockSize.value])
-        }
+        let slices = padded.slice(chunkSize: blockSize.value)
         
         // And finally convert each slice into an integer again.
         return slices.map { UInt(fromBase2: $0) }
     }
     
     public func reconstruct(_ blocks: [Block]) -> Data? {
-        return nil // TODO
+        // If any of the blocks uses more than `blockSize` bits in its base-2 representation,
+        // these blocks were not created using `ECB.chop` with the same `blockSize`.
+        let maxBlock = (1 << blockSize.value)
+        guard blocks.first(where: { $0 >= maxBlock }) == nil else { return nil }
+        
+        let padded = Array(blocks.map { $0.base2() }.map { $0 + Array<Bit>(repeating: .zero, count: blockSize.value - $0.count) }.joined())
+        
+        // If there is no bit equal to 1, or if the last 1 is followed by more than `blockSize - 1` many 0s,
+        // these blocks were not created using `ECB.chop` with the same `blockSize`.
+        guard let lastOne = padded.index(ofLast: .one), padded.count - lastOne <= blockSize.value else { return nil }
+        
+        // If after removing the padding the length of the remaining array of bits is not a multiple of 8,
+        // these blocks were not created using `ECB.chop`.
+        let bits = Array(padded[0 ..< lastOne])
+        guard bits.count % 8 == 0 else { return nil }
+        
+        let slices = bits.slice(chunkSize: 8)
+        
+        return Data(bytes: slices.map { UInt8(fromBase2: $0) })
+    }
+}
+
+extension Array where Element == Bit {
+    func slice(chunkSize: Int) -> [[Bit]] {
+        return stride(from: 0, to: count, by: chunkSize).map { index in
+            Array(self[index ..< index + chunkSize])
+        }
+    }
+    
+    func index(ofLast bit: Bit) -> Index? {
+        return reversed().index(of: bit).map({ count - ($0 + 1) })
     }
 }
--- TextbookRSATests/ECB/ECBTests.swift
+++ TextbookRSATests/ECB/ECBTests.swift
@@ -42,4 +42,86 @@ class ECBTests: XCTestCase {
             1
         ])
     }
+    
+    func testArraySlice() {
+        typealias Bit = TextbookRSA.Bit
+        
+        func slicesAreEqual(left: [[Bit]], right: [[Bit]]) -> Bool {
+            guard left.count == right.count else { return false }
+            
+            for index in left.indices where left[index] != right[index] {
+                return false
+            }
+            
+            return true
+        }
+        
+        XCTAssertTrue(slicesAreEqual(left: [Bit]().slice(chunkSize: 5), right: []))
+        XCTAssertTrue(slicesAreEqual(left: [Bit.one, .one, .zero, .zero, .one].slice(chunkSize: 5), right: [[.one, .one, .zero, .zero, .one]]))
+        XCTAssertTrue(slicesAreEqual(
+            left: [Bit.zero, .zero, .zero, .zero, .zero, .one, .one, .one, .one, .one].slice(chunkSize: 5),
+            right: [[.zero, .zero, .zero, .zero, .zero], [.one, .one, .one, .one, .one]]))
+    }
+    
+    func testArrayIndexOfLast() {
+        typealias Bit = TextbookRSA.Bit
+        
+        XCTAssertEqual([Bit]().index(ofLast: .one), nil)
+        XCTAssertEqual([Bit.zero, .zero, .zero].index(ofLast: .one), nil)
+        XCTAssertEqual([Bit.zero, .one, .zero, .one, .one, .zero, .zero].index(ofLast: .one), 4)
+    }
+    
+    func testReconstruct() {
+        /*
+         Test unsuccessful cases.
+         */
+        
+        let ecb7 = ECB(blockSize: try! Math.Positive(7))
+        
+        XCTAssertNil(ecb7.reconstruct([])) // No 1s
+        XCTAssertNil(ecb7.reconstruct([0, 0, 0])) // No 1s
+        XCTAssertNil(ecb7.reconstruct([0, 0b1000000, 0])) // Last 1 is followed by too many 0s.
+        XCTAssertNil(ecb7.reconstruct([0, 0, 1])) // Padding is correct, but after removing it we have 14 bits (not divisible by 8).
+        XCTAssertNil(ecb7.reconstruct([0b10000000, 0, 4])) // Padding is correct, but one block is too large.
+        
+        let ecb4 = ECB(blockSize: try! Math.Positive(4))
+        
+        XCTAssertNil(ecb4.reconstruct([])) // No 1s
+        XCTAssertNil(ecb4.reconstruct([0, 0, 0])) // No 1s
+        XCTAssertNil(ecb4.reconstruct([0, 0b1000, 0])) // Last 1 is followed by too many 0s.
+        XCTAssertNil(ecb4.reconstruct([0, 0, 0b0010])) // Padding is correct, but after removing it we have 9 bits (not divisible by 8).
+        XCTAssertNil(ecb4.reconstruct([0b10000, 0, 1])) // Padding is correct, but one block is too large.
+        
+        let ecb1 = ECB(blockSize: try! Math.Positive(1))
+        
+        XCTAssertNil(ecb1.reconstruct([])) // No 1s
+        XCTAssertNil(ecb1.reconstruct([0, 0, 0])) // No 1s
+        XCTAssertNil(ecb1.reconstruct([0, 1, 0])) // Last 1 is followed by too many 0s.
+        XCTAssertNil(ecb1.reconstruct([0, 0, 1])) // Padding is correct, but after removing it we have 2 bits (not divisible by 8).
+        XCTAssertNil(ecb1.reconstruct([0, 0, 0, 0, 0, 2, 0, 0, 1])) // Padding is correct, but one block is too large.
+        
+        /*
+         Test successful cases by using `ECB.chop` first.
+         */
+        
+        let emptyData = Data(bytes: [])
+        let oneByte42 = Data(bytes: [42])
+        let oneByteMax = Data(bytes: [UInt8.max])
+        let sixBytes = Data(bytes: [0b10010010, 0b11010011, 0b00000110, 0b11110010, 0b10011010, 0b10000010])
+        
+        XCTAssertEqual(ecb7.reconstruct(ecb7.chop(emptyData)), emptyData)
+        XCTAssertEqual(ecb7.reconstruct(ecb7.chop(oneByte42)), oneByte42)
+        XCTAssertEqual(ecb7.reconstruct(ecb7.chop(oneByteMax)), oneByteMax)
+        XCTAssertEqual(ecb7.reconstruct(ecb7.chop(sixBytes)), sixBytes)
+        
+        XCTAssertEqual(ecb4.reconstruct(ecb4.chop(emptyData)), emptyData)
+        XCTAssertEqual(ecb4.reconstruct(ecb4.chop(oneByte42)), oneByte42)
+        XCTAssertEqual(ecb4.reconstruct(ecb4.chop(oneByteMax)), oneByteMax)
+        XCTAssertEqual(ecb4.reconstruct(ecb4.chop(sixBytes)), sixBytes)
+        
+        XCTAssertEqual(ecb1.reconstruct(ecb1.chop(emptyData)), emptyData)
+        XCTAssertEqual(ecb1.reconstruct(ecb1.chop(oneByte42)), oneByte42)
+        XCTAssertEqual(ecb1.reconstruct(ecb1.chop(oneByteMax)), oneByteMax)
+        XCTAssertEqual(ecb1.reconstruct(ecb1.chop(sixBytes)), sixBytes)
+    }
 }

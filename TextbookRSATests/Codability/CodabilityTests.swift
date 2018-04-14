d3c1906b80f309641bc0a569531f6ce1c993e48e
Tomas Silveira Salles <ts@imagineon.de>
2018-04-14 09:43:02 +0200

    Implemented tests for encoding/decoding keys.

--- TextbookRSATests/Codability/CodabilityTests.swift
+++ TextbookRSATests/Codability/CodabilityTests.swift
@@ -65,11 +65,87 @@ class CodabilityTests: XCTestCase {
     }
     
     func testDecodingKeys() {
-        // TODO
+        let validJsonString = """
+            {
+                "prime_p": 5,
+                "prime_q": 7
+            }
+        """
+        
+        let validJsonData = validJsonString.data(using: .utf8)!
+        let validKeys = try? JSONDecoder().decode(RSA.Keys.self, from: validJsonData)
+        XCTAssertNotNil(validKeys)
+        XCTAssertEqual(validKeys?.private.p.value, 5)
+        XCTAssertEqual(validKeys?.private.q.value, 7)
+        
+        let jsonStringNoQ = """
+            {
+                "prime_p": 5
+            }
+        """
+        
+        let jsonDataNoQ = jsonStringNoQ.data(using: .utf8)!
+        XCTAssertThrowsError(try JSONDecoder().decode(RSA.Keys.self, from: jsonDataNoQ))
+        
+        let jsonStringNoP = """
+            {
+                "prime_q": 7
+            }
+        """
+        
+        let jsonDataNoP = jsonStringNoP.data(using: .utf8)!
+        XCTAssertThrowsError(try JSONDecoder().decode(RSA.Keys.self, from: jsonDataNoP))
+        
+        let jsonStringSmallP = """
+            {
+                "prime_p": 1,
+                "prime_q": 7
+            }
+        """
+        
+        let jsonDataSmallP = jsonStringSmallP.data(using: .utf8)!
+        XCTAssertThrowsError(try JSONDecoder().decode(RSA.Keys.self, from: jsonDataSmallP))
+        
+        let jsonStringSmallQ = """
+            {
+                "prime_p": 5,
+                "prime_q": 1
+            }
+        """
+        
+        let jsonDataSmallQ = jsonStringSmallQ.data(using: .utf8)!
+        XCTAssertThrowsError(try JSONDecoder().decode(RSA.Keys.self, from: jsonDataSmallQ))
+        
+        let jsonStringLargeFactors = """
+            {
+                "prime_p": \(RSAKeys.smallestPrimeFactorUpperBound + 1),
+                "prime_q": \(RSAKeys.smallestPrimeFactorUpperBound + 2)
+            }
+        """
+        
+        let jsonDataLargeFactors = jsonStringLargeFactors.data(using: .utf8)!
+        XCTAssertThrowsError(try JSONDecoder().decode(RSA.Keys.self, from: jsonDataLargeFactors))
+        
+        let jsonStringEqualFactors = """
+            {
+                "prime_p": 5,
+                "prime_q": 5
+            }
+        """
+        
+        let jsonDataEqualFactors = jsonStringEqualFactors.data(using: .utf8)!
+        XCTAssertThrowsError(try JSONDecoder().decode(RSA.Keys.self, from: jsonDataEqualFactors))
     }
     
     func testEncodingKeys() {
-        // TODO
+        let keys = try! RSA.Keys(privateP: try! .init(5), privateQ: try! .init(7))
+        let encoded = try? JSONEncoder().encode(keys)
+        XCTAssertNotNil(encoded)
+        
+        let decoded = encoded.map { try? JSONDecoder().decode(RSA.Keys.self, from: $0) }.flatMap { $0 }
+        XCTAssertNotNil(decoded)
+        XCTAssertEqual(decoded?.private.p.value, keys.private.p.value)
+        XCTAssertEqual(decoded?.private.q.value, keys.private.q.value)
     }
     
     func testDecodingEncryptedData() {

2a8e90726d6d73c55f513bb1989ff495899b1fca
Tomas Silveira Salles <ts@imagineon.de>
2018-04-14 02:53:55 +0200

    Added tests for encoding/decoding `RSATransformationParameters`.

--- TextbookRSATests/Codability/CodabilityTests.swift
+++ TextbookRSATests/Codability/CodabilityTests.swift
@@ -6,4 +6,52 @@
 //  Copyright © 2018 ImagineOn GmbH. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import TextbookRSA
+
+class CodabilityTests: XCTestCase {
+    func testDecodingTransformationParameters() {
+        let validJsonString = """
+            {
+                "modulo": 5,
+                "exponent": 7
+            }
+        """
+        
+        let validJsonData = validJsonString.data(using: .utf8)!
+        let validParms = try? JSONDecoder().decode(RSA.TransformationParameters.self, from: validJsonData)
+        XCTAssertNotNil(validParms)
+        XCTAssertEqual(validParms?.modulo.value, 5)
+        XCTAssertEqual(validParms?.exponent, 7)
+        
+        let jsonStringNoExponent = """
+            {
+                "modulo": 5
+            }
+        """
+        
+        let jsonDataNoExponent = jsonStringNoExponent.data(using: .utf8)!
+        XCTAssertThrowsError(try JSONDecoder().decode(RSA.TransformationParameters.self, from: jsonDataNoExponent))
+        
+        let jsonStringSmallModulo = """
+            {
+                "modulo": 1,
+                "exponent": 7
+            }
+        """
+        
+        let jsonDataSmallModulo = jsonStringSmallModulo.data(using: .utf8)!
+        XCTAssertThrowsError(try JSONDecoder().decode(RSA.TransformationParameters.self, from: jsonDataSmallModulo))
+    }
+    
+    func testEncodingTransformationParameters() {
+        let parms = RSA.TransformationParameters(modulo: try! .init(5), exponent: 7)
+        let encoded = try? JSONEncoder().encode(parms)
+        XCTAssertNotNil(encoded)
+        
+        let decoded = encoded.map { try? JSONDecoder().decode(RSA.TransformationParameters.self, from: $0) }.flatMap { $0 }
+        XCTAssertNotNil(decoded)
+        XCTAssertEqual(decoded?.modulo.value, parms.modulo.value)
+        XCTAssertEqual(decoded?.exponent, parms.exponent)
+    }
+}

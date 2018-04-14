a4883d3435c2abee12dc41de2a18802df5b44a46
Tomas Silveira Salles <ts@imagineon.de>
2018-04-14 02:33:47 +0200

    Added files for implementing `Codable` conformance and corresponding tests.

---
+++ TextbookRSA/Codability/Codability.swift
@@ -0,0 +1,9 @@
+//
+//  Codability.swift
+//  TextbookRSA
+//
+//  Created by Tomás Silveira Salles on 13.04.18.
+//  Copyright © 2018 ImagineOn GmbH. All rights reserved.
+//
+
+import Foundation
---
+++ TextbookRSATests/Codability/CodabilityTests.swift
@@ -0,0 +1,9 @@
+//
+//  CodabilityTests.swift
+//  TextbookRSATests
+//
+//  Created by Tomás Silveira Salles on 14.04.18.
+//  Copyright © 2018 ImagineOn GmbH. All rights reserved.
+//
+
+import Foundation

eeace6fd68115df5b58e0724ec2f3f6fd02caf15
Tomas Silveira Salles <ts@imagineon.de>
2018-04-16 11:04:00 +0200

    Added file for `PeriodDecrypter` tests.

---
+++ TextbookRSATests/Period/PeriodDecrypterTests.swift
@@ -0,0 +1,14 @@
+//
+//  PeriodDecrypterTests.swift
+//  TextbookRSATests
+//
+//  Created by Tomás Silveira Salles on 16.04.18.
+//  Copyright © 2018 ImagineOn GmbH. All rights reserved.
+//
+
+import XCTest
+@testable import TextbookRSA
+
+class PeriodDecrypterTests: XCTestCase {
+    
+}

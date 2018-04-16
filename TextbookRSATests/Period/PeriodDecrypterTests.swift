d9d7105848efa6eb289ec4633552e737bccd4829
Tomas Silveira Salles <ts@imagineon.de>
2018-04-16 11:22:13 +0200

    Added tests for `PeriodDecrypter.tryToExtractKeys`.

--- TextbookRSATests/Period/PeriodDecrypterTests.swift
+++ TextbookRSATests/Period/PeriodDecrypterTests.swift
@@ -10,5 +10,34 @@ import XCTest
 @testable import TextbookRSA
 
 class PeriodDecrypterTests: XCTestCase {
-    
+    func testTryToExtractKeys() {
+        for _ in 0 ..< 100 {
+            let keys = RSA.Keys()
+            let decrypter = PeriodDecrypter(publicKey: keys.public)
+        
+            let coprimeBlock: RSA.UInteger = {
+                var result: RSA.UInteger
+                
+                repeat {
+                    result = RSA.UInteger.randomInRange(min: 0, count: try! .init(UInt32(keys.public.value)))
+                } while !Math.areCoprime(result, keys.public.value)
+                
+                return result
+            }()
+        
+            XCTAssertNil(decrypter.tryToExtractKeys(from: coprimeBlock), "keys: \(keys) -- coprimeBlock: \(coprimeBlock)")
+        
+            let revealingBlock = keys.private.p.value * (keys.private.q.value / 2)
+        
+            guard let revealedKeys = decrypter.tryToExtractKeys(from: revealingBlock) else {
+                XCTFail("Could not extract keys even though the block was not coprime with the public key. - keys: \(keys) -- revealingBlock: \(revealingBlock)")
+                continue
+            }
+
+            let keysMatch =
+                (revealedKeys.private.p.value == keys.private.p.value && revealedKeys.private.q.value == keys.private.q.value)
+                || (revealedKeys.private.p.value == keys.private.q.value && revealedKeys.private.q.value == keys.private.p.value)
+            XCTAssertTrue(keysMatch, "keys: \(keys) -- revealingBlock: \(revealingBlock) -- revealedKeys: \(revealedKeys)")
+        }
+    }
 }

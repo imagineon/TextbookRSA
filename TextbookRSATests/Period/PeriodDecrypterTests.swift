3a31678ac80039cf808a63c8f8bd10ecfbb4d75b
Tomas Silveira Salles <ts@imagineon.de>
2018-04-16 12:11:33 +0200

    Added tests for `PeriodDecrypter.decrypt(... period:)`.

--- TextbookRSATests/Period/PeriodDecrypterTests.swift
+++ TextbookRSATests/Period/PeriodDecrypterTests.swift
@@ -10,21 +10,22 @@ import XCTest
 @testable import TextbookRSA
 
 class PeriodDecrypterTests: XCTestCase {
+    private static func block(coprimeTo publicKey: Math.Positive<UInt32>) -> RSA.UInteger {
+        var result: RSA.UInteger
+        
+        repeat {
+            result = RSA.UInteger.randomInRange(min: 0, count: publicKey)
+        } while !Math.areCoprime(result, UInt(publicKey.value))
+        
+        return result
+    }
+    
     func testTryToExtractKeys() {
         for _ in 0 ..< 100 {
             let keys = RSA.Keys()
             let decrypter = PeriodDecrypter(publicKey: keys.public)
         
-            let coprimeBlock: RSA.UInteger = {
-                var result: RSA.UInteger
-                
-                repeat {
-                    result = RSA.UInteger.randomInRange(min: 0, count: try! .init(UInt32(keys.public.value)))
-                } while !Math.areCoprime(result, keys.public.value)
-                
-                return result
-            }()
-        
+            let coprimeBlock = PeriodDecrypterTests.block(coprimeTo: try! .init(UInt32(keys.public.value)))
             XCTAssertNil(decrypter.tryToExtractKeys(from: coprimeBlock), "keys: \(keys) -- coprimeBlock: \(coprimeBlock)")
         
             let revealingBlock = keys.private.p.value * (keys.private.q.value / 2)
@@ -38,6 +39,42 @@ class PeriodDecrypterTests: XCTestCase {
                 (revealedKeys.private.p.value == keys.private.p.value && revealedKeys.private.q.value == keys.private.q.value)
                 || (revealedKeys.private.p.value == keys.private.q.value && revealedKeys.private.q.value == keys.private.p.value)
             XCTAssertTrue(keysMatch, "keys: \(keys) -- revealingBlock: \(revealingBlock) -- revealedKeys: \(revealedKeys)")
+        }
+    }
+    
+    private static func smallKeys() -> RSA.Keys {
+        var result: RSA.Keys?
+
+        repeat {
+            let min = UInt(2)
+            let max = UInt(100)
+            
+            let privateP = UInt.randomPrimeInRange(min: min, count: try! Math.Positive(UInt32(max - min)))
+            let privateQ = UInt.randomPrimeInRange(min: min, count: try! Math.Positive(UInt32(max - min)))
+            
+            result = try? RSA.Keys(privateP: privateP, privateQ: privateQ)
+        } while result == nil
+        
+        return result!
+    }
+    
+    func testDecryptBlockWithPeriod() {
+        for _ in 0 ..< 100 {
+            let keys = PeriodDecrypterTests.smallKeys() // We cannot run tests with ordinary keys because period finding is very very hard!
+            let decrypter = PeriodDecrypter(publicKey: keys.public)
+            
+            let block = PeriodDecrypterTests.block(coprimeTo: try! .init(UInt32(keys.public.value)))
+            
+            guard let period = PeriodOracle.period(of: block, modulo: decrypter.publicKey.positive) else {
+                XCTFail("Could not get period even though the block was coprime with the public key. - keys: \(keys) -- block: \(block)")
+                continue
+            }
+            
+            let encryptionParms = keys.generateEncryptionParameters()
+            let encryptedBlock = RSA.transform(block, with: encryptionParms)
+            let decryptedBlock = decrypter.decrypt(block: encryptedBlock, usedEncryptionExponent: encryptionParms.exponent, period: period)
+            
+            XCTAssertEqual(decryptedBlock, block, "keys: \(keys) -- block: \(block) -- period: \(period) -- encryptionParms: \(encryptionParms) -- encryptedBlock: \(encryptedBlock) -- decryptedBlock: \(String(describing: decryptedBlock))")
         }
     }
 }

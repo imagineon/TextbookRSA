f95f33be883d0ba48bf1f4674161292d65629a2c
Tomas Silveira Salles <ts@imagineon.de>
2018-04-16 13:02:43 +0200

    Added tests for full decryption using `PeriodDecrypter`.
    Almost identical to the tests used for `Decrypter`, but forcing the keys to be small.

--- TextbookRSATests/Period/PeriodDecrypterTests.swift
+++ TextbookRSATests/Period/PeriodDecrypterTests.swift
@@ -123,4 +123,39 @@ class PeriodDecrypterTests: XCTestCase {
             XCTAssertTrue(successOnZeroBlock, "Did not handle case `block == 0` correctly. - keys: \(keys) -- encryptionParms: \(encryptionParms) -- decryptedZeroBlock: \(String(describing: decryptedZeroBlock))")
         }
     }
+    
+    func testEncryptAndDecryptData() {
+        for iteration in 0 ..< 100 {
+            let keys = PeriodDecrypterTests.smallKeys() // We cannot run tests with ordinary keys because period finding is very very hard!
+            let decrypter = PeriodDecrypter(publicKey: keys.public)
+            let message = (iteration < 10) ? Data(bytes: []) :
+                Data(bytes: (0 ..< 10).map { _ in UInt8(arc4random_uniform(UInt32(UInt8.max) + 1)) })
+            let encryptedMessage = Encrypter.encrypt(message, parameters: keys.generateEncryptionParameters())
+            let decryptedMessage = decrypter.decrypt(encryptedMessage)
+            
+            XCTAssertEqual(message, decryptedMessage, "decrypter: \(decrypter) -- message: \(message.map { $0 }) -- encryptedMessage: \(encryptedMessage) -- decryptedMessage: \(String(describing: decryptedMessage?.map { $0 }))")
+        }
+    }
+    
+    func testEncryptAndDecryptText() {
+        for iteration in 0 ..< 100 {
+            let keys = PeriodDecrypterTests.smallKeys() // We cannot run tests with ordinary keys because period finding is very very hard!
+            let decrypter = PeriodDecrypter(publicKey: keys.public)
+            let message: String = {
+                switch iteration {
+                case 0 ..< 10:
+                    return ""
+                case 10 ..< 20:
+                    return "😎✌️🦊"
+                default:
+                    return "The quick brown fox jumps over the lazy dog"
+                }
+            }()
+            
+            let encryptedMessage = Encrypter.encrypt(message, parameters: keys.generateEncryptionParameters())
+            let decryptedMessage = decrypter.decryptText(encryptedMessage)
+            
+            XCTAssertEqual(message, decryptedMessage, "decrypter: \(decrypter) -- message: \(message) -- encryptedMessage: \(encryptedMessage) -- decryptedMessage: \(String(describing: decryptedMessage))")
+        }
+    }
 }

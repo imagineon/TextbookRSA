a93b0ea500fedbe9b6c1e3378264a343a1014653
Tomas Silveira Salles <ts@imagineon.de>
2018-04-18 11:07:08 +0200

    Added Swift Package Manager manifest ("Package.swift").

---
+++ Package.swift
@@ -0,0 +1,16 @@
+//
+//  Package.swift
+//  TextbookRSA
+//
+//  Created by Tomás Silveira Salles on 18.04.18.
+//  Copyright © 2018 ImagineOn GmbH. All rights reserved.
+//
+//  swift-tools-version:4.0
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "TextbookRSA",
+    exclude: ["TextbookRSATests"]
+)
